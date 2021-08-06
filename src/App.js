992c8520df834b4a099ee9cf05ce5e38ed30438d
sharonwirtzer <sharonwirtzer@gmail.com>
2021-08-06 20:01:14 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Navigasion from './screens/navigationBar/navigationBar'
+
+function App() {
+  return (
+    <div className="App">
+      <Navigasion/>
+    </div>
+  );
+}
+
+export default App;
