1c586d6ba66eeaa4b34262e2fc333bd7f97dab43
sharonwirtzer <sharonwirtzer@gmail.com>
2021-08-08 22:47:32 +0300

    color change

--- src/screens/aboutMe/AboutMe.jsx
+++ src/screens/aboutMe/AboutMe.jsx
@@ -9,8 +9,8 @@ function AboutMe() {
             <img src={person} alt='person icon'/>
             </div>
             <div className='about_text'>
-                <h1>ABOUT ME</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <h1 style={{ color: 'rgb(218, 218, 218)' }}>ABOUT ME</h1>
+                <p style={{ color: 'rgb(218, 218, 218)' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </div>
         </div>
     )
--- src/screens/header/Header.js
+++ src/screens/header/Header.js
@@ -5,21 +5,24 @@ import './Header.css'
 function Header() {
     return (
         <div className='main-info'>
-         <h1>What am i?</h1>
-         <Typed
-         strings={[
-             '',
-             '123',
-             '456',
-             '789',
-             '191112',
-             '',
-             '']}
-             typeSpeed={40}
-             backSpeed={50}
-             loop
-        />
-         
+            <h1>Hello, I'm  <h1 style={{ color: 'green' }}> Sharon Wirtzer </h1> {/* I'm a Full-Stack web developer. */}</h1>
+            <Typed
+                strings={[
+                    '',
+                    'A Software Engineer',
+                    'Also Known as an',
+                    'Front-end developer',
+                    'Back-end developer',
+                    'Full-Stack developer',
+                    'Or!',
+                    '"a Coder"',
+                    '',
+                    '']}
+                typeSpeed={40}
+                backSpeed={50}
+                loop
+            />
+
         </div>
     )
 }
