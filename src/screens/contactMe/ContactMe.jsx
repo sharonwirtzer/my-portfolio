5084269c442ff3225d0cb807b87882e484e07419
sharonwirtzer <sharonwirtzer@gmail.com>
2021-08-10 18:06:23 +0300

    href home

--- src/screens/contactMe/ContactMe.jsx
+++ src/screens/contactMe/ContactMe.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import './contactMe.css'
-import { backHome } from '../../helpers/navigationLinks'
 import { SocialIcon } from 'react-social-icons';
 import arrow from '../../images/arrow.png'
+
 
 
 
@@ -13,11 +13,11 @@ function ContactMe() {
                 <div>
                     <p>FIND ME ONLINE</p>
                     <SocialIcon url='https://www.linkedin.com/in/sharon-wirtzer/' style={{ height: 40, width: 40 }} />
-                    <SocialIcon url='https://github.com/sharonwirtzer' bgColor="#d0d9e3" style={{ height: 40, width: 40, marginLeft:'30px' }} />
-                    <SocialIcon url='sharonwirtzer@gmail.com' network="email" style={{ height: 40, width: 40, marginLeft:'30px'}} />
-                    <a style={{ marginLeft: '100px'}} href={backHome}> <img src={arrow} alt='arrow icon'/> </a>
-                </div>      
-             
+                    <SocialIcon url='https://github.com/sharonwirtzer' bgColor="#d0d9e3" style={{ height: 40, width: 40, marginLeft: '30px' }} />
+                    <SocialIcon url='sharonwirtzer@gmail.com' network="email" style={{ height: 40, width: 40, marginLeft: '30px' }} />
+                    <a style={{ marginLeft: '100px' }} href='#home'> <img src={arrow} alt='arrow icon' /> </a>
+                </div>
+
             </div>
             <div style={{ textAlign: 'center' }}>Copyright&copy; {new Date().getFullYear()} All rights reserved</div>
         </div>
