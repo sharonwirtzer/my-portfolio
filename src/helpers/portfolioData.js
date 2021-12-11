4b7ee2cb84afe02051186905695bb5ac91d420fa
sharonwirtzer <77779083+sharonwirtzer@users.noreply.github.com>
2021-12-11 18:48:52 +0200

    Add files via upload

---
+++ src/helpers/portfolioData.js
@@ -0,0 +1,46 @@
+import Instagrammi from '../images/projectsicon/Instagrammi.png'
+import Appsus from '../images/projectsicon/Appsus.png'
+import MineSweeeper from '../images/projectsicon/MineSweeper.png' 
+import MemeMaker from '../images/projectsicon/MemeMaker.png'
+import Calculator from '../images/projectsicon/calculator.png'
+
+const portfolioData=[
+    {
+        image: Instagrammi,
+        link:'http://instagrammi.herokuapp.com/',
+        title:'Instagrammi',
+        summary:'Instagram clone using Vue.js',
+        tech:'Vue.js | Sass | Node.js | MongoDB | Websockets'
+    },
+    {
+        image: Appsus,
+        link:'https://sharonwirtzer.github.io/sprint-3/#/',
+        title:'Appsus',
+        summary:'app for keeping things (reference app "google keep")',
+        tech:'Vue.js | HTML | CSS'
+    },
+     {
+        image: MineSweeeper,
+        link:'https://sharonwirtzer.github.io/sprint1/',
+        title:'Mine Sweeeper',
+        summary:'Minesweeper is a vintage single-player puzzle video game.',
+        tech:'Vanilla JS | HTML | Canvas | CSS'
+    }, 
+    {
+        image: MemeMaker,
+        link:'https://sharonwirtzer.github.io/sprint-2/',
+        title:'Meme Maker.',
+        summary:'A meme generator, allowing the user to upload their own pictures and afterwards download, save or publish their creation.',
+        tech:'Vanilla JS | HTML | CSS'
+    },
+    {
+        image: Calculator,
+        link:'https://sharonwirtzer.github.io/calculator/',
+        title:'calculator.',
+        summary:' mac calculator clone using Vanilla JS HTML and CSS',
+        tech:'Vanilla JS | HTML | CSS'
+    },
+]
+
+
+export default portfolioData
---
+++ src/helpers/techs.js
@@ -0,0 +1,34 @@
+export const techs =[
+    {
+        name:'React',
+        iconName:'react'
+    },
+    {
+        name:'Github',
+        iconName:'github_badge'
+    },
+    {
+        name:'Python',
+        iconName:'python'
+    },
+    {
+        name:'HTML5',
+        iconName:'html5'
+    },
+    {
+        name:'Javascript',
+        iconName:'javascript'
+    },
+    {
+        name:'Mongo DB',
+        iconName:'mongodb'
+    },
+    {
+        name:'CSS 3',
+        iconName:'css3'
+    },
+    {
+        name:'Bootstrap',
+        iconName:'bootstrap'
+    },
+]
